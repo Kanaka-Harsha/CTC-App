5e82f802e1d90f55e4715d6b65843cbdade36b2a
Kanakadandila Harsha <harshakanakadandila@gmail.com>
2026-05-23 19:57:58 +0530

    DB Connecting Code

---
+++ CTC-BackEnd/app/core/database.py
@@ -0,0 +1,19 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_url=os.getenv('DATABASE_URL')
+
+def get_db_connection():
+    try:
+        conn=psycopg2.connect(db_url,cursor_factory=RealDictCursor)
+        # print("OK")
+        return conn
+    except Exception as e:
+        print(f"Unable To Connect To The Database, Error -> {e}")
+        raise e 
+
+# get_db_connection()
