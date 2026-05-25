a3889d4043a62d849240478c0964a0e9634481b3
Kanakadandila Harsha <harshakanakadandila@gmail.com>
2026-05-25 20:46:50 +0530

    Modified Cache Invalidation

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-S3_BUCKET=""
-CLOUDFRONT_DIST_ID=""
+S3_BUCKET="s3://ctc-ui-store-2026"
+CLOUDFRONT_DIST_ID="E27IX6OCLS02XF"
 
-# Define colors for attractive output
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 CYAN='\033[0;36m'
 YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
 echo -e "${CYAN}🚀 Starting Deployment Process...${NC}\n"
 
@@ -19,11 +18,11 @@ if npm run build; then
     echo -e "${GREEN}✅ Build Successful!${NC}\n"
     
     echo -e "${YELLOW}Step 2: ☁️  Uploading the code to S3 Bucket...${NC}"
-    if aws s3 sync dist/ $S3_BUCKET --delete; then
+    if aws.exe s3 sync dist/ $S3_BUCKET --delete; then
         echo -e "${GREEN}✅ Upload Successful!${NC}\n"
         
         echo -e "${YELLOW}Step 3: 🔄 Invalidating The CloudFront Cache...${NC}"
-        if aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DIST_ID --paths "/*"; then
+        if aws.exe cloudfront create-invalidation --distribution-id $CLOUDFRONT_DIST_ID --paths "/*" --no-cli-pager; then
             echo -e "\n${GREEN}🎉 Deployment Completely Successful! Your app is live. 🚀${NC}"
         else
             echo -e "${RED}❌ ERROR: Failed to invalidate CloudFront cache.${NC}"
